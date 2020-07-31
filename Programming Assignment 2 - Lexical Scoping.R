988a5663a9d6e076d44c5c9750869b83569b0046
CRAI-97 <69054888+CRAI-97@users.noreply.github.com>
2020-07-31 13:22:17 -0600

    Add files via upload

---
+++ Programming Assignment 2 - Lexical Scoping.R
@@ -0,0 +1,23 @@
+#make Cache Matrix function 
+makeCacheMatrix <- function(x = matrix()) 
+  inv <- NULL
+  set <- function(y)
+    x <<- y
+    inv <<- NULL
+  get <- function() x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+
+#cache Solve function 
+  cacheSolve <- function(x, ...) 
+    inv <- x$getInverse()
+    if(!is.null(inv))
+      message("getting cached data")
+      return(inv)
+    data <- x$get()
+    inv <- solve(data)
+    x$setInverse(inv)
+    inv      
+
+  
